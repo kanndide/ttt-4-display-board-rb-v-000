af0ea25ff19ff60f6b23cdac4c202fab3674af4e
William Kann <william.l.kann@gmail.com>
2017-11-21 11:52:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,17 +3,11 @@
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board(board)
-  line1 = "#{board[0]}|#{board[1]}|#{board[2]}"
-  line2 = "-----------"
-  line3 = "#{board[3]}|#{board[4]}|#{board[5]}"
-  line4 = "-----------"
-  line5 = "#{board[6]}|#{board[7]}|#{board[8]}"
-  puts line1
-  puts line2
-  puts line3
-  puts line4
-  puts line5
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
 display_board(board)
-display_board(board[0] = " X ")
